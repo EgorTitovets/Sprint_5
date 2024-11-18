9fabc4577bb1315c02555b3dbce198d3e6a9bcdf
Egor Titovets <pryanik0992@gmail.com>
2024-11-18 08:41:20 +0500

    Доработал автотесты: улучшил локаторы для стабильности, использовал уникальные атрибуты элементов для их идентификации.

--- locators.py
+++ locators.py
@@ -2,29 +2,36 @@ from selenium.webdriver.common.by import By
 
 
 class TestLocators:
-    LOGIN_MAIN_PAGE = [By.XPATH, "/html/body/div[1]/div/main/section[2]/div/button"]  # кнопка "Войти в аккаунт" на главной
-    LOGIN_PERSONAL_ACCOUNT = [By.XPATH, "/html/body/div[1]/div/header/nav/a"] # кнопка "Личный кабинет" на главной
-    LOGIN_IN_REGISTRATION_FORM = [By.XPATH, "/html/body/div[1]/div/main/div/div/p/a"] # кнопка "Войти" в форме регистрации
-    LOGIN_IN_PASSWORD_PASSWORD_RECOVERY_FORM = [By.XPATH, "/html/body/div[1]/div/main/div/div/p/a"] # кнопка "Войти" в форме восстановления пароля
-    LOGIN_FINISH_BUTTON = [By.XPATH, "/html/body/div[1]/div/main/div/form/button"] # кнопка "Войти" в форме авторизации (при вводе Email и Пароль)
+    LOGIN_MAIN_PAGE = [By.XPATH, "//button[contains(@class, 'button_button_type_primary') and text()='Войти в аккаунт']"]  # кнопка "Войти в аккаунт" на главной
+    LOGIN_PERSONAL_ACCOUNT = [By.XPATH, "//p[contains(@class, 'AppHeader_header') and text()='Личный Кабинет']"] # кнопка "Личный кабинет" на главной
+    LOGIN_IN_REGISTRATION_FORM = [By.XPATH, "//a[contains(@class , 'Auth_link') and text() = 'Войти']"] # кнопка "Войти" в форме регистрации
+    LOGIN_IN_PASSWORD_PASSWORD_RECOVERY_FORM = [By.XPATH, "//a[contains(@class , 'Auth_link') and text() = 'Войти']"] # кнопка "Войти" в форме восстановления пароля
+    LOGIN_FINISH_BUTTON = [By.XPATH, "//button[contains(@class, 'button_button_type_primary') and text()='Войти']"] # кнопка "Войти" в форме авторизации (при вводе Email и Пароль)
 
-    LOGOUT_BUTTON = [By.XPATH, "/html/body/div[1]/div/main/div/nav/ul/li[3]/button"] # кнопка "Выйти"
+    LOGOUT_BUTTON = [By.XPATH, "//button[@type='button' and text()='Выход']"] # кнопка "Выйти"
 
-    REGISTRATION_START_BUTTON = [By.XPATH, "/html/body/div[1]/div/main/div/div/p[1]/a"] # кнопка "Зарегистрироваться" в разделе авторизации (для открытия формы регистрации)
-    REGISTRATION_FINISH_BUTTON = [By.XPATH, "/html/body/div[1]/div/main/div/form/button"] # кнопка "Зарегистрироваться" в форме регистрации
+    REGISTRATION_START_BUTTON = [By.XPATH, "//a[@href='/register' and text()='Зарегистрироваться']"] # кнопка "Зарегистрироваться" в разделе авторизации (для открытия формы регистрации)
+    REGISTRATION_FINISH_BUTTON = [By.XPATH, "//button[text()='Зарегистрироваться']"] # кнопка "Зарегистрироваться" в форме регистрации
 
-    NAME_INPUT_FIELD_REGISTRATION = [By.XPATH, "/html/body/div[1]/div/main/div/form/fieldset[1]/div/div/input"] # поле ввода Имя в регистрации
-    EMAIL_INPUT_FIELD_REGISTRATION = [By.XPATH, "/html/body/div[1]/div/main/div/form/fieldset[2]/div/div/input"] # поле ввода email в регистрации
-    PASSWORD_INPUT_FIELD_REGISTRATION = [By.XPATH, "/html/body/div[1]/div/main/div/form/fieldset[3]/div/div/input"] # поле вода пароля в регистрации
+    NAME_INPUT_FIELD_REGISTRATION = [By.XPATH, "//label[text()='Имя']/following-sibling::input"] # поле ввода Имя в регистрации
+    EMAIL_INPUT_FIELD_REGISTRATION = [By.XPATH, "//label[text()='Email']/following-sibling::input"] # поле ввода email в регистрации
+    PASSWORD_INPUT_FIELD_REGISTRATION = [By.XPATH, "//input[@type='password' and @name='Пароль']"] # поле вода пароля в регистрации
 
-    EMAIL_INPUT_FIELD_AUTHORIZATION = [By.XPATH, '/html/body/div[1]/div/main/div/form/fieldset[1]/div/div/input'] # поле ввода email в авторизации
-    PASSWORD_INPUT_FIELD_AUTHORIZATION = [By.XPATH, "/html/body/div[1]/div/main/div/form/fieldset[2]/div/div/input"] # поле ввода email в авторизации
+    EMAIL_INPUT_FIELD_AUTHORIZATION = [By.XPATH, "//label[text()='Email']/following-sibling::input"] # поле ввода email в авторизации
+    PASSWORD_INPUT_FIELD_AUTHORIZATION = [By.XPATH, "//input[@type='password' and @name='Пароль']"] # поле ввода email в авторизации
 
-    PLACE_ORDER_BUTTON = [By.XPATH, "/html/body/div[1]/div/main/section[2]/div/button"] # кнопка "Оформить заказ" у авторизованного пользователя
+    PLACE_ORDER_BUTTON = [By.XPATH, "//*[text()='Оформить заказ']"] # кнопка "Оформить заказ" у авторизованного пользователя
 
-    PASSWORD_RECOVERY_BUTTON = [By.XPATH, "/html/body/div[1]/div/main/div/div/p[2]/a"] # "Восстановить пароль" в форме авторизации
+    PASSWORD_RECOVERY_BUTTON = [By.XPATH, "//a[text()='Восстановить пароль']"] # "Восстановить пароль" в форме авторизации
 
-    CONSTRUCTOR_BUTTON = [By.XPATH, "/html/body/div/div/header/nav/ul/li[1]/a/p"] # "Конструктор" в хэдере
-    CONSTRUCTOR_GENERAL_FORM = [By.XPATH, "/html/body/div[1]/div/main/section[1]/h1"] # Общая/главная страница Конструктора с информингом "Соберите бургер"
+    CONSTRUCTOR_BUTTON = [By.XPATH, "//p[contains(@class, 'AppHeader_header') and text()='Конструктор']"] # "Конструктор" в хэдере
+    CONSTRUCTOR_GENERAL_FORM = [By.XPATH, "//h1[contains(@class, 'text_type_main-large') and text()='Соберите бургер']"] # Общая/главная страница Конструктора с информингом "Соберите бургер"
 
-    STELLAR_BURGERS_BUTTON = [By.XPATH, "//*[@id='root']/div/header/nav/div/a"] # логотип Stellar Burgers
+    STELLAR_BURGERS_BUTTON = [By.XPATH, "//a/*[name()='svg']"] # логотип Stellar Burgers
+
+    SAUCES_IN_CONSTRUCTOR = [By.XPATH, "//*[@class='text text_type_main-default' and text()='Соусы']"] # раздел Соусы в конструкторе
+    SAUCES_HEADER_SECTION = [By.XPATH, "//*[@class='text text_type_main-medium mb-6 mt-10' and text()='Соусы']"] # Хэдер секции Соусы в конструкторе
+    BUNS_IN_CONSTRUCTOR = [By.XPATH, "//*[@class='text text_type_main-default' and text()='Булки']"] # раздел Булки в конструкторе
+    BUNS_HEADER_SECTION = [By.XPATH, "//*[@class='text text_type_main-medium mb-6 mt-10' and text()='Булки']"] # Хэдер секции Соусы в конструкторе
+    FILLINGS_IN_CONSTRUCTOR = [By.XPATH, "//*[@class='text text_type_main-default' and text()='Начинки']"]  # раздел Начинки в конструкторе
+    FILLINGS_HEADER_SECTION = [By.XPATH,"//*[@class='text text_type_main-medium mb-6 mt-10' and text()='Начинки']"]  # Хэдер секции Соусы в конструкторе
--- tests/test_constructor.py
+++ tests/test_constructor.py
@@ -1,26 +1,26 @@
-from selenium.webdriver.common.by import By
+from locators import TestLocators
 
 
 class TestRedirectConstructor:
 
     def test_redirect_to_sauces_section(self, driver):
-        sauces_section = driver.find_element(By.XPATH, "/html/body/div[1]/div/main/section[1]/div[1]/div[2]/span")
+        sauces_section = driver.find_element(*TestLocators.SAUCES_IN_CONSTRUCTOR)
         sauces_section.click()
         driver.execute_script("arguments[0].scrollIntoView();", sauces_section)
-        sauces_header = driver.find_element(By.XPATH, "//*[text()='Соусы']")
+        sauces_header = driver.find_element(*TestLocators.SAUCES_HEADER_SECTION)
         assert sauces_header.is_displayed()
 
     def test_redirect_to_buns_section(self, driver):
-        driver.find_element(By.XPATH, "/html/body/div[1]/div/main/section[1]/div[1]/div[2]/span").click()
-        buns_section = driver.find_element(By.XPATH, "/html/body/div[1]/div/main/section[1]/div[1]/div[1]/span")
+        driver.find_element(*TestLocators.SAUCES_IN_CONSTRUCTOR).click()
+        buns_section = driver.find_element(*TestLocators.BUNS_IN_CONSTRUCTOR)
         buns_section.click()
         driver.execute_script("arguments[0].scrollIntoView();", buns_section)
-        buns_section = driver.find_element(By.XPATH, "//*[text()='Булки']")
-        assert buns_section.is_displayed()
+        buns_header = driver.find_element(*TestLocators.SAUCES_HEADER_SECTION)
+        assert buns_header.is_displayed()
 
     def test_redirect_to_fillings_section(self, driver):
-        fillings_section = driver.find_element(By.XPATH, "/html/body/div[1]/div/main/section[1]/div[1]/div[3]/span")
+        fillings_section = driver.find_element(*TestLocators.FILLINGS_IN_CONSTRUCTOR)
         fillings_section.click()
         driver.execute_script("arguments[0].scrollIntoView();", fillings_section)
-        fillings_section = driver.find_element(By.XPATH, "//*[text()='Начинки']")
-        assert fillings_section.is_displayed()
+        fillings_header = driver.find_element(*TestLocators.FILLINGS_HEADER_SECTION)
+        assert fillings_header.is_displayed()
